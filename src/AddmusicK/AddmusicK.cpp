3034a220c90b71ae09dd44d07e98692c3a078570
nyanpasu64 <nyanpasu64@tuta.io>
2022-02-09 22:48:21 -0800

    Remove unused variable pointersPos

--- src/AddmusicK/AddmusicK.cpp
+++ src/AddmusicK/AddmusicK.cpp
@@ -1136,7 +1136,6 @@ void fixMusicPointers()
 	if (verbose)
 		std::cout << "Fixing song pointers..." << std::endl;
 
-	int pointersPos = programSize + 0x400;
 	std::stringstream globalPointers;
 	std::stringstream incbins;
 
