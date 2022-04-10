675ef1fffb2a6238ee62ea56e333160274e9706c
KungFuFurby <KungFuFurby@gmail.com>
2022-04-10 14:19:34 -0400

    Correct a bug with using the wrong variable reference for a zero tempo check
    The source code I got made a mistake with testing with ltempo, when ltempo was
    not the variable that was supposed to be checked. Also, AddmusicK used a tabbed
    block without using brackets, which for me is more Python-esque than C++-esque.

--- src/AddmusicK/Music.cpp
+++ src/AddmusicK/Music.cpp
@@ -765,11 +765,10 @@ void Music::parseTempoCommand()
 	
 	tempo = divideByTempoRatio(ltempo, false);
 	
-	if (ltempo == 0) {
+	if (tempo == 0) {
 		error("Tempo has been zeroed out by #halvetempo");
+		tempo = ltempo;
 	}
-
-	tempo = ltempo;
 
 	if (duration == -1) {
 		tempoDefined = true;
