1b62d6e75df0dde85442cf27c15edaee67b14a99
KungFuFurby <KungFuFurby@gmail.com>
2022-05-22 16:12:35 -0400

    Try to build AddmusicK command line application with MinGW - Try 12
    Typo corrections

--- build_script.ps1
+++ build_script.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = "Stop"
-Write_Output $Env:PATH
+Write-Output $Env:PATH
 $Env:PATH += ";C:\MinGW\bin\"
 # msbuild "C:\projects\addmusickff\src\AddmusicK.sln" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 # if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
 # ls C:\MinGW\bin\
 mingw32-make
-$ if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
