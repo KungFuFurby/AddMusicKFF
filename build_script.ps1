29c2b5ba8fdcdf41722affcbb31cab6725bf9ee8
KungFuFurby <KungFuFurby@gmail.com>
2022-05-22 15:23:15 -0400

    Try to build AddmusicK command line application with MinGW - Try 7

--- build_script.ps1
+++ build_script.ps1
@@ -1,4 +1,5 @@
 # msbuild "C:\projects\addmusickff\src\AddmusicK.sln" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 # if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
-make
+ls C:\MinGW\bin\
+C:\MinGW\bin\make
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
