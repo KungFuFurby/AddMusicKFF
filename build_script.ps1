42c53655317e4d6aeeeeac872a78d5daf856970f
KungFuFurby <KungFuFurby@gmail.com>
2022-05-22 16:47:38 -0400

    Try to build AddmusicK command line application with MinGW - Try 18
    OK, we don't use plain 'ole make again. Ah well, try two for MinGW-w64.

--- build_script.ps1
+++ build_script.ps1
@@ -3,5 +3,5 @@ $Env:PATH += ";C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin"
 # msbuild "C:\projects\addmusickff\src\AddmusicK.sln" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 # if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
 ls C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin
-make
+mingw32-make
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
