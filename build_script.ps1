5b79d374cf1fbec5fc5bb9a0772f65b1892caeb0
KungFuFurby <KungFuFurby@gmail.com>
2022-05-22 17:20:48 -0400

    Try to build AddmusicK command line application with MinGW - Try 20
    Try that again, this time through MSYS2's bash shell. Also, don't try to compile
    again using that other MinGW-w64 distribution for now: this is just a config
    check, and we need to give Appveyor a break from trying to compile the same
    failed thing over and over again.

--- build_script.ps1
+++ build_script.ps1
@@ -1,8 +1,10 @@
 Write-Output $Env:PATH
-$Env:PATH += ";C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin"
-pacman -Q
+$Env:PATH += ";C:\msys64\usr\bin\"
+ls C:\msys64\usr\bin\
+bash -lc "pacman -Q"
 # msbuild "C:\projects\addmusickff\src\AddmusicK.sln" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 # if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
-ls C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin
-mingw32-make
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+# ls C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin
+# mingw32-make
+# if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+$host.SetShouldExit(1)
