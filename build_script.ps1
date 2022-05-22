6bdfa51929344cb77ae988f8cc0dcc30e9d782d0
KungFuFurby <KungFuFurby@gmail.com>
2022-05-22 16:39:50 -0400

    Try to build AddmusicK command line application with MinGW - Try 16
    Don't waste Appveyor's time at the moment. We're using ls to look for the
    correct directories...

--- build_script.ps1
+++ build_script.ps1
@@ -2,6 +2,7 @@ Write-Output $Env:PATH
 $Env:PATH += ";C:\MinGW\bin\"
 # msbuild "C:\projects\addmusickff\src\AddmusicK.sln" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 # if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
-ls C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0
-mingw32-make
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+ls C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\*
+$host.SetShouldExit(1)
+# mingw32-make
+# if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
