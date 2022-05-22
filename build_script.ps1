102cc822a7829814a195712718ebb1155a8879dc
KungFuFurby <KungFuFurby@gmail.com>
2022-05-22 15:36:06 -0400

    Try to build AddmusicK command line application with MinGW - Try 9
    Let's see if a separate build script entry will do the trick better than putting
    it into Powershell...

--- build_script.ps1
+++ build_script.ps1
@@ -1,6 +1,6 @@
-$ErrorActionPreference = "Stop"
-# msbuild "C:\projects\addmusickff\src\AddmusicK.sln" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
-# if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
-ls C:\MinGW\bin\
-mingw32-make
+# $ErrorActionPreference = "Stop"
+msbuild "C:\projects\addmusickff\src\AddmusicK.sln" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+# ls C:\MinGW\bin\
+# mingw32-make
+$ if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
