c58de0b497448884f922a1a74d46eb2cf644acbc
KungFuFurby <KungFuFurby@gmail.com>
2022-05-22 17:15:26 -0400

    Try to build AddmusicK command line application with MinGW - Try 19
    More of a configuration check. Going to try to have MSYS2 deal with this more
    directly... and possibly with an update to it on the fly.

--- build_script.ps1
+++ build_script.ps1
@@ -1,5 +1,6 @@
 Write-Output $Env:PATH
 $Env:PATH += ";C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin"
+pacman -Q
 # msbuild "C:\projects\addmusickff\src\AddmusicK.sln" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 # if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
 ls C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin
