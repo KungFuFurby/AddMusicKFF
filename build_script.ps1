8091fd6980455791569cfe1b3fae09f2c463b8db
KungFuFurby <KungFuFurby@gmail.com>
2022-05-22 23:03:54 -0400

    Try to build AddmusicK command line application with MinGW - Try 22
    Check what's going on with cygwin, citing a cygheap base mismatch.

--- build_script.ps1
+++ build_script.ps1
@@ -1,8 +1,10 @@
 Write-Output $Env:PATH
-$Env:PATH += ";C:\msys64\usr\bin\"
-ls C:\msys64\usr\bin\
+# $Env:PATH += ";C:\msys64\usr\bin\"
+#ls C:\msys64\usr\bin\
+ls C:\cygwin\bin
 # msbuild "C:\projects\addmusickff\src\AddmusicK.sln" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 # if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
 # ls C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin
-make
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+# make
+# if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+$host.SetShouldExit(1)
