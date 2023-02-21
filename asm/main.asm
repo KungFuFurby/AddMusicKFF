a76f188c616181647cd51d70efe6fa659ef55e15
KungFuFurby <KungFuFurby@gmail.com>
2023-02-20 19:01:55 -0500

    Optimize out a jmp opcode to SubC_1D
    This can instead be turned into a bra opcode courtesy of a relocation.

--- asm/main.asm
+++ asm/main.asm
@@ -2540,6 +2540,13 @@ KeyOnVoices:
 	tset	!PlayingVoices, a
 	ret
 
+SubC_1D:
+if !noSFX = !false
+	call	TerminateIfSFXPlaying
+endif
+	mov	a, $48
+	bra	KeyOnVoices
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 cmdF0:					; Echo off
 {
