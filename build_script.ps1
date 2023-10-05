77145a5c0b722f00fe23af182fc237dc885af1cb
KungFuFurby <KungFuFurby@gmail.com>
2023-10-05 14:11:43 -0400

    Revert zip_script and build_script to prior to AddmusicK1.0.9 branch merger

--- build_script.ps1
+++ build_script.ps1
@@ -1,5 +1,5 @@
 $Env:PATH += ";C:\cygwin\bin\"
 msbuild "C:\projects\addmusickff\src\AddmusicK.sln" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
-#make
-#if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
+make
+if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
