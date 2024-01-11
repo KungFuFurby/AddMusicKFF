63da94209ca5e091ea527615086f7e9bfd393f39
KungFuFurby <KungFuFurby@gmail.com>
2024-01-11 11:21:35 -0500

    Rename the .zip file and folder for compilation of AddmusicK 1.0.10 beta

--- zip_script.ps1
+++ zip_script.ps1
@@ -10,6 +10,6 @@ del src\AM405Remover\Release -Recurse
 del src\AMKGUI\obj -Recurse
 del src\Release -Recurse
 7z a src.zip src
-mkdir "AddmusicK_1.0.10"
-move 1DF9, 1DFC, asm, music, readme_files, samples, SPCs, stats, Visualizations, Addmusic_list.txt, "Addmusic_sample groups.txt", "Addmusic_sound effects.txt", addmusicMRemover.pl, readme.html, src.zip, *.exe, *.dll, Makefile "AddmusicK_1.0.10"
-7z a "AddmusicK1.0.10_latestRC.zip" "AddmusicK_1.0.10"
+mkdir "AddmusicK_1.0.10_Beta"
+move 1DF9, 1DFC, asm, music, readme_files, samples, SPCs, stats, Visualizations, Addmusic_list.txt, "Addmusic_sample groups.txt", "Addmusic_sound effects.txt", addmusicMRemover.pl, readme.html, src.zip, *.exe, *.dll, Makefile "AddmusicK_1.0.10_Beta"
+7z a "AddmusicK1.0.10_latestRCbeta.zip" "AddmusicK_1.0.10_Beta"
