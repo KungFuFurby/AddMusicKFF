dbc2c075eb87655f517d362b7652eff704c7e351
KungFuFurby <KungFuFurby@gmail.com>
2024-03-12 15:40:09 -0400

    Revert zip_script and build_script to prior to AddmusicK1.0.10 branch merger

--- zip_script.ps1
+++ zip_script.ps1
@@ -1,4 +1,4 @@
-move src\Release\AddmusicK.exe	AddmusicK.exe
+move src\Release\AddmusicK.exe	AddmusicKVS2019.exe
 move src\Release\AM4Batch.exe	AM4Batch.exe
 move src\Release\AMKGUI.exe		AMKGUI.exe
 move src\Release\AMMBatch.exe	AMMBatch.exe
@@ -10,6 +10,6 @@ del src\AM405Remover\Release -Recurse
 del src\AMKGUI\obj -Recurse
 del src\Release -Recurse
 7z a src.zip src
-mkdir "AddmusicK_1.0.10"
-move 1DF9, 1DFC, asm, music, readme_files, samples, SPCs, stats, Visualizations, Addmusic_list.txt, "Addmusic_sample groups.txt", "Addmusic_sound effects.txt", addmusicMRemover.pl, readme.html, src.zip, *.exe, *.dll, Makefile "AddmusicK_1.0.10"
-7z a "AddmusicK1.0.10_latestRC.zip" "AddmusicK_1.0.10"
+mkdir "AddmusicKFF"
+move 1DF9, 1DFC, asm, music, readme_files, samples, SPCs, stats, Visualizations, Addmusic_list.txt, "Addmusic_sample groups.txt", "Addmusic_sound effects.txt", addmusicMRemover.pl, readme.html, src.zip, *.exe, *.dll, Makefile "AddmusicKFF"
+7z a "AMKFF_latest.zip" "AddmusicKFF"
