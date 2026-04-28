8e915528e879caee6f274adf70f30fb75823efc8
KungFuFurby <KungFuFurby@gmail.com>
2026-04-27 21:59:37 -0400

    Update build scripts for Version X

--- zip_script.ps1
+++ zip_script.ps1
@@ -10,6 +10,6 @@ del src\AM405Remover\Release -Recurse
 del src\AMKGUI\obj -Recurse
 del src\Release -Recurse
 7z a src.zip src
-mkdir "AddmusicK_1.0.12_Beta"
-move 1DF9, 1DFC, asm, music, readme_files, samples, SPCs, stats, Visualizations, Addmusic_list.txt, "Addmusic_sample groups.txt", "Addmusic_sound effects.txt", addmusicMRemover.pl, readme.html, src.zip, *.exe, *.dll, Makefile "AddmusicK_1.0.12_Beta"
-7z a "AddmusicK1.0.12_latestRCbeta.zip" "AddmusicK_1.0.12_Beta"
+mkdir "AddmusicK_1.0.12_Beta_Version_X"
+move 1DF9, 1DFC, asm, music, readme_files, samples, SPCs, stats, Visualizations, Addmusic_list.txt, "Addmusic_sample groups.txt", "Addmusic_sound effects.txt", addmusicMRemover.pl, readme.html, src.zip, *.exe, *.dll, Makefile "AddmusicK_1.0.12_Beta_Version_X"
+7z a "AddmusicK1.0.12_latestRCversionX.zip" "AddmusicK_1.0.12_Beta_Version_X"
